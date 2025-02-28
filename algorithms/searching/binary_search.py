6448676b96879519837c4b98ceef5eea20260403
Elizabeth Martin <elma5454@colorado.edu>
2025-02-28 15:40:17 -0600

    Updated binary search

--- algorithms/searching/binary_search.py
+++ algorithms/searching/binary_search.py
@@ -1,35 +1,63 @@
 class Solution:
-    """Performs binary search in a sorted array recursively."""
+    """Performs binary search in a sorted array."""
 
     # Analysis: time = O(log n), space = O(log n)
-    def search_in_sorted(self,arr: list[int], k: int, left: int = 0, right: int = None) -> bool:
-        """Recursively search for k in a sorted array using binary search."""
+    def rec_bin_search(self,arr: list[int], k: int, left: int = 0, right: int = None) -> int:
+        """
+        Returns the index of the target value in an integer array.
+        Searches using recursive binary search.
+        """
 
         if right is None: # Catch first run with default input
             right = len(arr) - 1
         
         if left > right:
-            return False
+            return -1
         
         mid = (left + right) // 2
 
         if arr[mid] == k:
-            return True
+            return mid
         
         # Recursive calls for right & left side (respectively)
         # Note: avoid passing sliced array as it creates new arrays and adds to space complexity
         if arr[mid] < k:
-            return self.search_in_sorted(arr, k, mid + 1, right)
+            return self.rec_bin_search(arr, k, mid + 1, right)
         else:
-            return self.search_in_sorted(arr, k, left, mid - 1)
+            return self.rec_bin_search(arr, k, left, mid - 1)
+
+    # Analysis: time = O(log n), space = O(1)
+    def bin_search(self, nums: list[int], target: int) -> int:
+        """
+        Returns the index of the target value in an integer array.
+        Searches using binary search.
+        """
+        n = len(nums)
+        left, right = 0, n - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1
 
 
 if __name__ == "__main__":
-    nums1 = [1, 2, 3, 4, 5, 6]
-    k = 6
-    print(f"Nums1: {nums1}, k = {k}.  Present in array: {Solution().search_in_sorted(nums1, k)}")
+    solution = Solution()
+    test_cases = [
+        ([1, 2, 3, 4, 5, 6], 6, 5),
+        ([2, 5, 6, 9, 12], 4, -1),
+        ([-1, 0, 3, 5, 9, 12], 9, 4),
+        ([-1, 0, 3, 5, 9, 12], 2, -1)
+    ]
 
-    nums2 = [2, 5, 6, 9, 12]
-    k = 4
-    print(f"Nums2: {nums2}, k = {k}.  Present in array: {Solution().search_in_sorted(nums2, k)}")
+    for i, (test, target, expected) in enumerate(test_cases):
+        assert (it_output := solution.bin_search(test, target)) == expected, f"Iterative test case {i + 1} failed, expected: {expected}, got: {it_output}"
+        assert (rec_output := solution.rec_bin_search(test, target)) == expected, f"Recursive test case {i + 1} failed, expected: {expected}, got: {rec_output}"
 
+    print("All test cases passed!")
