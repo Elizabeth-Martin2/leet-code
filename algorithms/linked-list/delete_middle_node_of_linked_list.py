adecb154053922133a58e5e580b802910f9428da
Elizabeth Martin <elma5454@colorado.edu>
2025-03-19 19:33:56 -0500

    middle node

--- algorithms/linked-list/delete_middle_node_of_linked_list.py
+++ algorithms/linked-list/delete_middle_node_of_linked_list.py
@@ -17,3 +17,16 @@ class Solution:
         prev.next = slow.next
 
         return head
+
+
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+
+        fast = slow = head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+
+        return slow
