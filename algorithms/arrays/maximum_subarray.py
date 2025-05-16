36533e1ecfcb807db7a491d38ae6a37eb5eb0a92
Elizabeth Martin <elma5454@colorado.edu>
2025-05-16 13:26:36 -0500

    Added debugging

--- algorithms/arrays/maximum_subarray.py
+++ algorithms/arrays/maximum_subarray.py
@@ -1,12 +1,19 @@
 class Solution:
     """Finds the maximum subarray sum using Kadane's Algorithm."""
 
+    DEBUG = False
+    @staticmethod
+    def _debug(*args, **kwargs):
+        if Solution.DEBUG:
+            print(*args, **kwargs)
+
     # Analysis: time = O(n), space = O(1)
     def max_sub_array(self, nums: list[int]) -> int:
         """Returns the sum of the maximum subarray."""
         current_sum = max_sum = nums[0]
-
+        
         for num in nums[1:]:
+            Solution._debug(f"current_sum: {current_sum}, max_sum: {max_sum}, num: {num}")
             current_sum = max(current_sum + num, num)
             max_sum = max(current_sum, max_sum)
 
